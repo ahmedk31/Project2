026828aa34c3bac144817dab422c61552c6c1f2d
ahmedk31 <94133401+ahmedk31@users.noreply.github.com>
2024-03-26 16:53:50 -0400

    Add files via upload

---
+++ user2.py
@@ -0,0 +1,51 @@
+def add_doctor(doctors, name, specialization):
+    new_id = str(len(doctors) + 1)
+    new_doctor = {
+        "id": new_id,
+        "name": name,
+        "specialization": specialization
+    }
+    doctors.append(new_doctor)
+    return new_doctor
+
+def add_patient(patients, name, age, gender, doctor_id, room_number, bed_number):
+    new_id = str(len(patients) + 1)
+    new_patient = {
+        "id": new_id,
+        "name": name,
+        "age": age,
+        "gender": gender,
+        "doctorId": doctor_id,
+        "roomNumber": room_number,
+        "bedNumber": bed_number
+    }
+    patients.append(new_patient)
+    return new_patient
+
+
+
+def test_add_doctor_and_patient():
+    # Setup: Empty lists simulating JSON data
+    doctors = []
+    patients = []
+
+    # Test adding a doctor
+    new_doctor = add_doctor(doctors, "Dr. X", "Surgeon")
+    assert new_doctor in doctors, "The doctor should be added to the list"
+    assert new_doctor['name'] == "Dr. X", "The doctor's name should be Dr. Test"
+    assert new_doctor['specialization'] == "Surgeon", "The doctor's specialization should be General"
+
+    # Test adding a patient
+    new_patient = add_patient(patients, "Patient X", 30, "Female", "1", "101", "1B")
+    assert new_patient in patients, "The patient should be added to the list"
+    assert new_patient['name'] == "Patient X", "The patient's name should be Patient Test"
+    assert new_patient['age'] == 30, "The patient's age should be 30"
+    assert new_patient['gender'] == "Female", "The patient's gender should be Female"
+    assert new_patient['doctorId'] == "1", "The patient's doctorId should be 1"
+    assert new_patient['roomNumber'] == "101", "The patient's roomNumber should be 101"
+    assert new_patient['bedNumber'] == "1B", "The patient's bedNumber should be 1B"
+
+    print("All tests passed!")
+
+# Run the test
+test_add_doctor_and_patient()
